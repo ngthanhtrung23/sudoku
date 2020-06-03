18d6988c95fc13e841fcd0d67bc578aa24c14074
Trung Nguyen <ngthanhtrung23@gmail.com>
2020-06-03 14:51:11 +0800

    Draw Sudoku board.

---
+++ src/data.js
@@ -0,0 +1,53 @@
+class CellData {
+    constructor(_row, _col) {
+        this.value = null;
+        this.cornerValues = new Set();
+        this.centerValues = new Set();
+
+        this.row = _row;
+        this.col = _col;
+    }
+
+    isRegionTop() {
+        return this.row % 3 == 0;
+    }
+    isRegionBottom() {
+        return this.row % 3 == 2;
+    }
+    isRegionLeft() {
+        return this.col % 3 == 0;
+    }
+    isRegionRight() {
+        return this.col % 3 == 2;
+    }
+
+    isBoardTop() {
+        return this.row == 0;
+    }
+    isBoardBottom() {
+        return this.row == 8;
+    }
+    isBoardLeft() {
+        return this.col == 0;
+    }
+    isBoardRight() {
+        return this.col == 8;
+    }
+}
+
+class BoardData {
+    constructor() {
+        this.cells = [];
+        for (let i = 0; i < 81; i++) {
+            this.cells.push(new CellData(~~(i / 9), i % 9));
+        }
+    }
+}
+
+class GameData {
+    constructor() {
+        this.board = new BoardData();
+    }
+}
+
+export { CellData, BoardData, GameData };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './data.js';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { GameData } from './data.js';
+
+class Cell extends React.Component {
+    render() {
+        let classes = ['cell'];
+
+        if (this.props.cell.isRegionTop()) {
+            classes.push('region-top');
+        }
+        if (this.props.cell.isRegionLeft()) {
+            classes.push('region-left');
+        }
+
+        if (this.props.cell.isBoardLeft()) {
+            classes.push('board-left');
+        }
+        if (this.props.cell.isBoardRight()) {
+            classes.push('board-right');
+        }
+        if (this.props.cell.isBoardTop()) {
+            classes.push('board-top');
+        }
+        if (this.props.cell.isBoardBottom()) {
+            classes.push('board-bottom');
+        }
+
+        return (
+            <div class={classes.join(' ')}></div>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderCell(i) {
+        return (
+            <Cell
+                cell={this.props.board.cells[i]}
+            />
+        );
+    }
+
+    renderRow(startingCell) {
+        let cells = [];
+        for (let i = startingCell; i < startingCell + 9; i++) {
+            cells.push(this.renderCell(i));
+        }
+        return (
+            <div class="row">
+                {cells}
+            </div>
+        );
+    }
+
+    render() {
+        let rows = [];
+        for (let i = 0; i < 81; i += 9) {
+            rows.push(this.renderRow(i));
+        }
+        return (
+            <div>
+                {rows}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = new GameData();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Sudoku Tool</h1>
+                <Board board={this.state.board} />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
