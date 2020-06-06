72e146e705f4c830e95ea7550498ea096ff4dad3
ngthanhtrung23 <ngthanhtrung23@gmail.com>
2020-06-06 17:37:28 +0000

    Deploying to gh-pages from  @ 40bd727b41051d2ec646d7a9da16873faab9d5c4 🚀

---
+++ precache-manifest.5e0d2644e9d27fc89fec065d36d61ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bf15d9870fc5aa3cb2d7b6aa8ef90b",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "d0515c7677bdd82c6e4d",
+    "url": "/sudoku/static/css/main.3d4ee589.chunk.css"
+  },
+  {
+    "revision": "ca5b80e64a10bb442b5f",
+    "url": "/sudoku/static/js/2.e6267f1e.chunk.js"
+  },
+  {
+    "revision": "f08ec97a32e768d388de705e90edbcf2",
+    "url": "/sudoku/static/js/2.e6267f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0515c7677bdd82c6e4d",
+    "url": "/sudoku/static/js/main.adb0364b.chunk.js"
+  },
+  {
+    "revision": "5e467a82d5680057536a",
+    "url": "/sudoku/static/js/runtime-main.e006c59b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sudoku/precache-manifest.6714cb4205bbb32eccfc5aaeb2a9244a.js"
+  "/sudoku/precache-manifest.5e0d2644e9d27fc89fec065d36d61ca1.js"
 );
 
 self.addEventListener('message', (event) => {
