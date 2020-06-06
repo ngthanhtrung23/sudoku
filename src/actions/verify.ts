d696d4a33c5d01e8db46d8dedf326a77f76b1948
Trung Nguyen <ngthanhtrung23@gmail.com>
2020-06-07 01:43:41 +0800

    Remove empty file.

--- src/actions/verify.ts
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash';
-
-import { ActionTypes } from './types';
-import { BoardModel } from '../models/board';
-import { ControlModel } from '../models/control';
-import { updateBoard } from './board';
